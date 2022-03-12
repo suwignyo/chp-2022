f21f03acdf6fdf28333ad827012b4f4c66f380bb
suwignyo <gsuwignyo@gmail.com>
2022-03-12 15:45:18 -0500

    allow to resend email

--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -10,6 +10,7 @@ import {
   InputGroup,
   InputRightElement,
   Grid,
+  Link,
 } from "@chakra-ui/react";
 import { useState, useRef } from "react";
 import { useForm } from "react-hook-form";
@@ -31,6 +32,20 @@ const initialValues = {
   phoneNumber: "",
 };
 
+const Subscribed = () => (
+  <Text color="white" textAlign="center">
+    Thank you for saving the date! We will send updates to your email. If you
+    have a specific question, please{" "}
+    <Link
+      href={`mailto:${`gerchelle.2022@gmail.com`}`}
+      textDecoration="underline"
+    >
+      email
+    </Link>{" "}
+    us.
+  </Text>
+);
+
 export const Subscribe = (props) => {
   const {
     register,
@@ -38,14 +53,23 @@ export const Subscribe = (props) => {
     setError,
     formState: { errors, isSubmitting },
     reset,
+    getValues,
   } = useForm<FormInputs>({
     shouldUseNativeValidation: true,
     defaultValues: initialValues,
   });
   const reRef = useRef<ReCAPTCHA>();
 
-  const [guestName, setGuestName] = useState<string | null>(null);
-  const [phoneNumber, setPhoneNumber] = useState<string | null>(null);
+  const [alreadyRegistered, setAlreadyRegistered] = useState<boolean>(false);
+  const [currentGuest, setCurrentGuest] = useState<Record<string, any> | null>(
+    null
+  );
+
+  // const getGuest = async (email: string) => {
+  //   const { data, error } = await supabase.from("guest").select();
+  //   console.log("data", data);
+  // };
+
   const onSubmit = async (formData: FormInputs) => {
     const token = await reRef.current.executeAsync();
     reRef.current.reset();
@@ -58,26 +82,31 @@ export const Subscribe = (props) => {
         phoneNumber: formData.phoneNumber,
       },
     ]);
+    if (error.message.includes("duplicate")) {
+      return setAlreadyRegistered(true);
+    }
     if (error) {
+      console.log("error", error);
       return setError("firstName", {
         message:
           "There was an issue submitting your information, please try a different email address or phone number",
-        // "Invalid token, please email gerrchelle.2022@gmail.com for more information",
       });
     }
-    setGuestName(`${guest[0].firstName} ${guest[0].lastName}`);
+    setCurrentGuest(guest[0]);
     sendMail({ ...formData, token: token });
   };
 
   const toast = useToast();
 
-  const Subscribed = () => (
-    <Text color="white">
-      Thank you for saving the date! We will send updates to your email!
-    </Text>
-  );
-
   const formInvalid = Object.keys(errors).length > 0;
+
+  const resendEmail = async () => {
+    const token = await reRef.current.executeAsync();
+    reRef.current.reset();
+    sendMail({ ...getValues(), token: token });
+    setCurrentGuest(getValues());
+    setAlreadyRegistered(false);
+  };
 
   const sendMail = async (data) => {
     try {
@@ -97,7 +126,7 @@ export const Subscribe = (props) => {
           duration: 9000,
           isClosable: true,
         });
-      }, 2000);
+      }, 1000);
     } catch (error) {
       // toast error message
     }
@@ -111,86 +140,117 @@ export const Subscribe = (props) => {
         ref={reRef}
       />
       <Box py="12">
-        {guestName ? (
-          <Subscribed />
-        ) : (
-          <Box data-aos="fade-up">
+        {alreadyRegistered ? (
+          <Box>
             <Text textAlign={"center"} color="white">
-              Please enter your information to get updates on our wedding.
+              We already received your information. If you haven&apos;t receive
+              the email, please click the button below. Or if you have a
+              specific question, please{" "}
+              <Link
+                href={`mailto:${`gerchelle.2022@gmail.com`}`}
+                textDecoration="underline"
+              >
+                email
+              </Link>{" "}
+              us.
             </Text>
-            <FormControl isInvalid={formInvalid}>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <Grid templateColumns="1fr 1fr" gap={3} mt={3}>
-                  <Input
-                    {...register("firstName", {
-                      required: "First name is required",
-                    })} // custom message
-                    placeholder="First name"
-                    type="text"
-                    color="white"
-                    borderRadius="0"
-                  />
-                  <Input
-                    {...register("lastName", {
-                      required: "Last name is required",
-                    })} // custom message
-                    placeholder="Last name"
-                    type="text"
-                    color="white"
-                    borderRadius="0"
-                  />
-                </Grid>
-                <Grid mt={3} templateColumns="1fr 1fr" gap={3}>
-                  <InputGroup>
-                    <Input
-                      {...register("email", {
-                        required: "Email is required",
-                      })} // custom message
-                      placeholder="Email"
-                      id="email"
-                      color="white"
-                      borderRadius="0"
-                    />
-                    <InputRightElement
-                      pointerEvents="none"
-                      children={<EmailIcon color="gray.300" />}
-                    />
-                  </InputGroup>
-                  <InputGroup>
-                    <Input
-                      type="tel"
-                      placeholder="Phone number"
-                      {...register("phoneNumber", {
-                        required: "Please enter phone number",
-                      })} // custom message
-                      color="white"
-                      borderRadius="0"
-                    />
-                    <InputRightElement
-                      pointerEvents="none"
-                      children={<PhoneIcon color="gray.300" />}
-                    />
-                  </InputGroup>
-                </Grid>
-
-                <FormErrorMessage>
-                  {errors?.firstName?.message}
-                </FormErrorMessage>
-                <FormErrorMessage>{errors?.email?.message}</FormErrorMessage>
-                <Flex justifyContent="center">
-                  <Button
-                    mt="4"
-                    type="submit"
-                    isLoading={isSubmitting}
-                    disabled={isSubmitting}
-                    borderRadius="0"
-                  >
-                    Save the Date!
-                  </Button>
-                </Flex>
-              </form>
-            </FormControl>
+            <Flex justifyContent="center">
+              <Button
+                mt="4"
+                type="button"
+                onClick={resendEmail}
+                borderRadius="0"
+              >
+                Send Email
+              </Button>
+            </Flex>
           </Box>
+        ) : (
+          <>
+            {currentGuest ? (
+              <Subscribed />
+            ) : (
+              <Box data-aos="fade-up">
+                <Text textAlign={"center"} color="white">
+                  Please enter your information to get updates on our wedding.
+                </Text>
+                <FormControl isInvalid={formInvalid}>
+                  <form onSubmit={handleSubmit(onSubmit)}>
+                    <Grid templateColumns="1fr 1fr" gap={3} mt={3}>
+                      <Input
+                        {...register("firstName", {
+                          required: "First name is required",
+                        })}
+                        placeholder="First name"
+                        type="text"
+                        color="white"
+                        borderRadius="0"
+                      />
+                      <Input
+                        {...register("lastName", {
+                          required: "Last name is required",
+                        })}
+                        placeholder="Last name"
+                        type="text"
+                        color="white"
+                        borderRadius="0"
+                      />
+                    </Grid>
+                    <Grid mt={3} templateColumns="1fr 1fr" gap={3}>
+                      <InputGroup>
+                        <Input
+                          {...register("email", {
+                            required: "Email is required",
+                          })}
+                          placeholder="Email"
+                          id="email"
+                          color="white"
+                          borderRadius="0"
+                        />
+                        <InputRightElement
+                          pointerEvents="none"
+                          children={<EmailIcon color="gray.300" />}
+                        />
+                      </InputGroup>
+                      <InputGroup>
+                        <Input
+                          type="tel"
+                          placeholder="Phone number"
+                          {...register("phoneNumber", {
+                            required: "Please enter phone number",
+                          })}
+                          color="white"
+                          borderRadius="0"
+                        />
+                        <InputRightElement
+                          pointerEvents="none"
+                          children={<PhoneIcon color="gray.300" />}
+                        />
+                      </InputGroup>
+                    </Grid>
+
+                    <FormErrorMessage>
+                      {errors?.firstName?.message}
+                    </FormErrorMessage>
+                    <FormErrorMessage>
+                      {errors?.email?.message}
+                    </FormErrorMessage>
+                    <Flex justifyContent="center">
+                      <Button
+                        mt="4"
+                        type="submit"
+                        isLoading={isSubmitting}
+                        disabled={isSubmitting}
+                        borderRadius="0"
+                      >
+                        Save the Date!
+                      </Button>
+                    </Flex>
+                  </form>
+                </FormControl>
+              </Box>
+            )}
+          </>
         )}
       </Box>
     </Box>
