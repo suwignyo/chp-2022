539e662138d887fd4d268607724f002159267dad
suwignyo <gsuwignyo@gmail.com>
2022-04-23 11:24:34 -0400

    fix var

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,10 +2,12 @@ import { Box, Button, Flex, FlexProps } from "@chakra-ui/react";
 import { useAuth } from "../util/useAuth";
 export const NavBar = ({ children }: FlexProps) => {
   const { logout, user } = useAuth();
-  console.log("user", user);
+
   return (
     <Flex flex={1} alignItems="center" justifyContent="flex-end">
-      <Box mr={2}>Hi, {user?.displayName ? user.displayName : user.email}</Box>
+      <Box mr={2}>
+        Hi, {user?.displayName ? user?.displayName : user?.email}
+      </Box>
       <Button type="button" onClick={logout}>
         Logout
       </Button>
