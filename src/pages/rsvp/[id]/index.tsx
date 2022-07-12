59d97700f1fee3a83e65f8c5c3f0d78b4ae2d29f
suwignyo <gsuwignyo@gmail.com>
2022-07-11 22:43:30 -0400

    setup RSVP page

---
+++ src/pages/rsvp/[id]/index.tsx
@@ -0,0 +1,46 @@
+import { Box, Flex } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { supabase } from "../../../util/supabaseClient";
+
+const Rsvp = ({ guest }) => {
+  console.log("guest", guest);
+  const router = useRouter();
+  console.log("router", router);
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <Flex flex={1} alignItems="center" justifyContent="flex-end">
+      <Box mr={2}>
+        Hi, {guest.firstName} {guest.lastName}
+      </Box>
+    </Flex>
+  );
+};
+
+export async function getStaticPaths() {
+  const { data, error } = await supabase.from("guest").select("uuid");
+  const paths = data.map((guest) => ({
+    params: { id: JSON.stringify(guest.uuid) },
+  }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { id } = params;
+  const { data } = await supabase
+    .from("guest")
+    .select()
+    .filter("uuid", "eq", id)
+    .single();
+  return {
+    props: {
+      guest: data,
+    },
+  };
+}
+
+export default Rsvp;
---
+++ src/pages/rsvp/index.tsx
@@ -0,0 +1,11 @@
+import { Box, Flex } from "@chakra-ui/react";
+
+const Rsvp = ({}) => {
+  return (
+    <Flex flex={1} alignItems="center" justifyContent="flex-end">
+      <Box mr={2}>RSVP index page</Box>
+    </Flex>
+  );
+};
+
+export default Rsvp;
