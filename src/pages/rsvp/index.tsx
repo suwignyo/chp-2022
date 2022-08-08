3f956059b3f3ab946b26a19d4d7fff80dd1ce5b5
Gerry Suwignyo <gsuwignyo@gmail.com>
2022-08-08 13:29:33 -0400

    separate has rsvp n havent

--- src/pages/rsvp/index.tsx
+++ src/pages/rsvp/index.tsx
@@ -3,6 +3,7 @@ import {
   Button,
   Flex,
   Grid,
+  Heading,
   Link,
   Switch,
   Text,
@@ -14,7 +15,6 @@ import { useAuth } from "../../util/useAuth";
 
 const Rsvp = ({}) => {
   const [guestsData, setGuestsData] = useState<any[]>([]);
-
   const getGuests = async () => {
     const { data, error } = await supabase.from("guest").select();
     setGuestsData(data);
@@ -95,55 +95,126 @@ const Rsvp = ({}) => {
     <>
       <Box mr={2}>RSVP index page</Box>
       <Grid>
+        <Heading textAlign={"center"}>HAS NOT RSVP&apos;ed</Heading>
         {guestsData?.map((guest, index) => {
           return (
-            <div key={guest.uuid}>
-              <Flex
-                justifyContent="center"
-                alignItems={"center"}
-                my={4}
-                boxShadow="2px 4px 8px rgba(0,0,0,0.2)"
-              >
-                <Text mr="4">{index + 1}</Text>
-                <Link href={`rsvp/${guest.uuid}`} textDecoration="underline">
-                  {guest.firstName} {guest.lastName}
-                </Link>
-                <Box px={8}>
-                  <Text>
-                    Can invite multiple:{" "}
-                    {guest.allowMultipleInvites ? "yes" : "no"}
-                  </Text>
-                  <Switch
-                    id="allowMultipleInvites"
-                    isChecked={guest.allowMultipleInvites}
-                    onChange={() =>
-                      onAllowMultipleSwitch(
-                        !guest.allowMultipleInvites,
-                        guest.id
-                      )
-                    }
-                  ></Switch>
-                </Box>
-                <Box px={8}>
-                  <Text>Email sent: {guest.emailSent} times</Text>
-                  <Button
-                    type="submit"
-                    onClick={() => {
-                      onInvite(
-                        guest.email,
-                        `${window.location.href}/${guest.uuid}`,
-                        guest.firstName,
-                        guest.lastName
-                      );
-                      onAddEmailSentTimes(guest.emailSent + 1, guest.id);
-                    }}
-                    borderRadius="0"
-                  >
-                    Send rsvp email
-                  </Button>
-                </Box>
-              </Flex>
-            </div>
+            <>
+              {!guest.rsvp && (
+                <Flex
+                  justifyContent="center"
+                  alignItems={"center"}
+                  my={4}
+                  key={guest.uuid}
+                >
+                  <Flex p={6} boxShadow="2px 4px 8px rgba(0,0,0,0.2)">
+                    <Text mr="4">{index + 1}</Text>
+                    <Link
+                      href={`rsvp/${guest.uuid}`}
+                      textDecoration="underline"
+                    >
+                      {guest.firstName} {guest.lastName}
+                    </Link>
+                    <Box px={8}>
+                      <Text>
+                        Can invite multiple:{" "}
+                        {guest.allowMultipleInvites ? "yes" : "no"}
+                      </Text>
+                      <Switch
+                        id="allowMultipleInvites"
+                        isChecked={guest.allowMultipleInvites}
+                        onChange={() =>
+                          onAllowMultipleSwitch(
+                            !guest.allowMultipleInvites,
+                            guest.id
+                          )
+                        }
+                      ></Switch>
+                    </Box>
+                    <Box px={8}>
+                      <Text>Email sent: {guest.emailSent} times</Text>
+                      <Button
+                        type="submit"
+                        onClick={() => {
+                          onInvite(
+                            guest.email,
+                            `${window.location.href}/${guest.uuid}`,
+                            guest.firstName,
+                            guest.lastName
+                          );
+                          onAddEmailSentTimes(guest.emailSent + 1, guest.id);
+                        }}
+                        borderRadius="0"
+                      >
+                        Send rsvp email
+                      </Button>
+                    </Box>
+                  </Flex>
+                </Flex>
+              )}
+            </>
+          );
+        })}
+
+        <Heading textAlign={"center"} mt={10}>
+          HAS RSVP&apos;ed
+        </Heading>
+
+        {guestsData?.map((guest, index) => {
+          return (
+            <>
+              {guest.rsvp && (
+                <Flex
+                  justifyContent="center"
+                  alignItems={"center"}
+                  my={4}
+                  key={guest.uuid}
+                >
+                  <Flex p={6} boxShadow="2px 4px 8px rgba(0,0,0,0.2)">
+                    <Text mr="4">{index + 1}</Text>
+                    <Link
+                      href={`rsvp/${guest.uuid}`}
+                      textDecoration="underline"
+                    >
+                      {guest.firstName} {guest.lastName}
+                    </Link>
+                    <Box px={8}>
+                      <Text>
+                        Can invite multiple:{" "}
+                        {guest.allowMultipleInvites ? "yes" : "no"}
+                      </Text>
+                      <Switch
+                        id="allowMultipleInvites"
+                        isChecked={guest.allowMultipleInvites}
+                        onChange={() =>
+                          onAllowMultipleSwitch(
+                            !guest.allowMultipleInvites,
+                            guest.id
+                          )
+                        }
+                      ></Switch>
+                    </Box>
+                    <Box px={8}>
+                      <Text>Email sent: {guest.emailSent} times</Text>
+                      <Button
+                        type="submit"
+                        onClick={() => {
+                          onInvite(
+                            guest.email,
+                            `${window.location.href}/${guest.uuid}`,
+                            guest.firstName,
+                            guest.lastName
+                          );
+                          onAddEmailSentTimes(guest.emailSent + 1, guest.id);
+                        }}
+                        borderRadius="0"
+                      >
+                        Send rsvp email
+                      </Button>
+                    </Box>
+                  </Flex>
+                </Flex>
+              )}
+            </>
           );
         })}
       </Grid>
